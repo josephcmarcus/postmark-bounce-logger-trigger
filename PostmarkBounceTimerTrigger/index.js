99e2e88d1108f6243ca48b80d1c7167c2e39a6b2
Joe Marcus <josephcmarcus@gmail.com>
2023-03-01 16:44:22 -0500

    Fix undefined date variable in context log

--- PostmarkBounceTimerTrigger/index.js
+++ PostmarkBounceTimerTrigger/index.js
@@ -1,3 +1,22 @@
-module.exports = async function (context, myTimer) {
+const dotenv = require('dotenv').config();
+const axios = require('axios');
 
+module.exports = async function (context, myTimer) {
+  let date = new Date();
+  try {
+    const response = await axios({
+      method: 'get',
+      url: `${process.env.URL}?code=${process.env.AUTH_CODE}`,
+      headers: {
+        Accept: '*/*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept-Language': 'en_US',
+      },
+    });
+    context.log(`Trigger succeeded at ${date}. ${response.data}`);
+    return response.data;
+  } catch (err) {
+    context.log(`Trigger failed at ${date}: ${err}`);
+  }
 };
